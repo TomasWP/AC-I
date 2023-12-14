d1d98764fe6a0f98c0a06c18dcf4fc6929d96e3b
TomasWP <tomasfonseca14@ua.pt>
2023-12-14 18:08:07 +0000

    Ficheiros Treino Teste AC-I

--- Exercicios Treino Teste/Teste 2/Aula 11/Ex 11.1.asm
+++ Exercicios Treino Teste/Teste 2/Aula 11/Ex 11.1.asm
@@ -7,6 +7,9 @@
 	.eqv	print_intu10, 36
 	.eqv	print_char, 11
 	.eqv	print_float, 2
+	.eqv	read_int, 5
+	.eqv	read_str, 8
+	.eqv	read_float, 6
 	.align 2	
 stg:	.word	72343
 	.asciiz	"Napoleao"
@@ -22,6 +25,31 @@ str3:	.asciiz	"\nNota: "
 		
 main:	
 	la	$t0, stg
+	
+	la	$a0, str1
+	li	$v0, print_str
+	syscall
+	li	$v0, read_int
+	syscall
+	sw	$v0, id_number($t0)
+	la	$a0, str2
+	li	$v0, print_str
+	syscall
+	addiu	$a0, $t0, first_name
+	li	$a1, 17
+	li	$v0, read_str
+	syscall	
+	addiu	$a0, $t0, last_name
+	li	$a1, 17
+	li	$v0, read_str
+	syscall	
+	la	$a0, str3
+	li	$v0, print_str
+	syscall
+	li	$v0, read_float
+	syscall
+	s.s	$f0, grade($t0)
+
 	la	$a0, str1
 	li	$v0, print_str
 	syscall
@@ -43,11 +71,7 @@ main:
 	la	$a0, str3
 	li	$v0, print_str
 	syscall
-	addiu 	$a0, $t0, grade
+	l.s	$f12, grade($t0)
 	li	$v0, print_float
 	syscall
 	jr	$ra
-	
-	
-
-		
